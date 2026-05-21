b540bc6a38c1d3a6df9749c88189eab7ba108d12
djangopk786 <djangopk786@gmail.com>
2026-05-21 16:12:13 +0500

    add

--- core/db.py
+++ core/db.py
@@ -1,6 +1,16 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
+# DATABASE_URL = "postgresql://postgres:Awais%238381@localhost/postgresql"
+# engine = create_engine(DATABASE_URL)
+# sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Awais%238381@localhost/postgresql"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
