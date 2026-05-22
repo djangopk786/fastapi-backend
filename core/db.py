63cb8ed540a537ee1d818d93eaee7ac93a131fde
djangopk786 <djangopk786@gmail.com>
2026-05-22 12:28:06 +0500

    adddbbb

--- core/db.py
+++ core/db.py
@@ -6,18 +6,3 @@ engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-# import os
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# if not DATABASE_URL:
-#     raise Exception("DATABASE_URL missing in Railway environment")
-
-# if DATABASE_URL.startswith("postgres://"):
-#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
-
-# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
-# SessionLocal = sessionmaker(bind=engine)
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.exc import SQLAlchemyError
 from routes.routes import router as category_router
 from core.db import engine
 from models import model
-
-
 
 
 class Application:
@@ -16,10 +15,6 @@ class Application:
             model.Base.metadata.create_all(bind=engine)
             print("Database connected successfully.")
 
-
-
-
-
         except SQLAlchemyError as error:
             print(f"Database connection failed: {error}")
 
@@ -27,6 +22,18 @@ class Application:
         self.app = FastAPI(
             title="Professional FastAPI App",
             version="1.0.0"
+        )
+
+        # ⭐ ADD CORS HERE (VERY IMPORTANT)
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=[
+                "http://localhost:5174",
+                "http://localhost:5173"
+            ],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
         )
 
         # Register routes
