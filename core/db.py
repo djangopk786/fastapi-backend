984d1d00ae2eac1bb3d5efce625566583d2c5e68
djangopk786 <djangopk786@gmail.com>
2026-05-22 10:47:28 +0500

    add

--- core/db.py
+++ core/db.py
@@ -1,23 +1,23 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = "postgresql://postgres:Awais%238381@localhost/postgresql"
-# engine = create_engine(DATABASE_URL)
-# sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:Awais%238381@localhost/postgresql"
+engine = create_engine(DATABASE_URL)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL missing in Railway environment")
 
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+# DATABASE_URL = os.getenv("DATABASE_URL")
 
-SessionLocal = sessionmaker(bind=engine)
+# if not DATABASE_URL:
+#     raise Exception("DATABASE_URL missing in Railway environment")
+
+# if DATABASE_URL.startswith("postgres://"):
+#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+
+# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+# SessionLocal = sessionmaker(bind=engine)
