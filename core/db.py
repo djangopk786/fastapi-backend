fa1dd78a9a0de56a33f827189d4ec6b3f53901d5
djangopk786 <djangopk786@gmail.com>
2026-05-22 12:11:54 +0500

    adddbb

--- core/db.py
+++ core/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Awais%238381@localhost/postgresql"
+DATABASE_URL = "postgresql://postgres:NRJrWXyzzcMQoLbFjTmNsKOVzVyaQqOw@kodama.proxy.rlwy.net:19550/railway"
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
